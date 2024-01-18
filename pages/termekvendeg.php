1d3f784d5821f3cc977da3caf7937dc5daaf79bc
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2024-01-18 09:10:46 +0100

    Add files via upload

---
+++ pages/termekvendeg.php
@@ -0,0 +1,33 @@
+<div class="container">
+    <div class="row">
+        <div class="card col-lg-3">
+            <img src="./Kepek/a_vasarlas/c/barna macska pulóver.jpg" alt="Denim Jeans" style="width:100%">
+            <h1>barna macska pulóver</h1>
+            <p class="price">5.000 Ft</p>
+            <p><button class="btn" onclick="vendeg()"><a href="index.php?menu=bejelentkezes">Kosár</a></button></p>
+        </div>
+        <div class="card col-lg-3">
+            <img src="./Kepek/a_vasarlas/c/macska hám.jpg" alt="Denim Jeans" style="width:100%">
+            <h1>macska hám</h1>
+            <p class="price">5.000 Ft</p>
+            <p><button class="btn" onclick="vendeg()"><a href="index.php?menu=bejelentkezes">Kosár</a></button></p>
+        </div>
+        <div class="card col-lg-3">
+            <img src="./Kepek/a_vasarlas/c/macska kendo.webp" alt="Denim Jeans" style="width:100%">
+            <h1>macska kendo</h1>
+            <p class="price">2.000 Ft</p>
+            <p><button class="btn" onclick="vendeg()"><a href="index.php?menu=bejelentkezes">Kosár</a></button></p>
+        </div>
+        <div class="card col-lg-3">
+            <img src="./Kepek/a_vasarlas/c/kék rózsaszín macska pulóver.jpeg" alt="Denim Jeans" style="width:100%">
+            <h1>kék rózsaszín macska pulóver</h1>
+            <p class="price">5.000 Ft</p>
+            <p><button class="btn" onclick="vendeg()"><a href="index.php?menu=bejelentkezes">Kosár</a></button></p>
+        </div>
+    </div>
+    <br>
+    <h2>Több termékért <a href="index.php?menu=bejelentkezes">jelentkezzen be</a></h2>
+</div>
+
+<script src="./js/ablak.js"></script>
+
