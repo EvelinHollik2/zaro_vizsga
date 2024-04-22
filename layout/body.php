8f17042deefb3d020438e6ef3c56ac791ee615ca
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2024-04-22 07:47:40 +0200

    Delete layout directory

--- layout/body.php
+++
@@ -1,8 +0,0 @@
-<body>
-
-    <?php
-    require_once './menu.php';
- ?>
-    <script src="./bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
--- layout/header.php
+++
@@ -1,1 +0,0 @@
-<a href="index.php?menu=home" class="logo"><h1 class="onmaga">Cats&Dogs</h1></a>
